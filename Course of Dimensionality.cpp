9cf02866f0095f86d9319ae357fb7527ffc8718c
andre <andre.alvarez@ucsp.edu.pe>
2020-09-01 22:24:28 -0500

    Add files via upload

---
+++ Course of Dimensionality.cpp
@@ -0,0 +1,12 @@
+﻿// Course of Dimensionality.cpp : Defines the entry point for the application.
+//
+
+#include "Course of Dimensionality.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
